2c1cf54f0b43c21ad3cff951e2822eb3d085567d
Maxime AUBURTIN <maxime.auburtin@dockermail.io>
2017-02-11 01:20:43 +0100

    Added all dependencies to package.json. Added code to index.ts and the new Express app file called app.ts.

--- init.sh
+++ init.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Run this script to init the project and then switch to Atom
-tsc --init
-npm init
-npm install @types/node
-npm install @types/express
+# tsc --init
+# npm init
+npm install @types/node --save-dev
+npm install @types/express --save-dev
+npm install @types/debug --save-dev
+npm install @types/body-parser --save-dev
+npm install @types/morgan --save-dev
 
 echo -e "\nProject ready...\n"
